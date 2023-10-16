9ab04f6590bb5091c0600a97a3474ee30ffca40e
SnowCoder404 <72316152+SnowCoder404@users.noreply.github.com>
2023-10-16 15:37:28 +0200

    Create fizz-buzz.py

---
+++ fizz-buzz.py
@@ -0,0 +1,11 @@
+zahl = 0
+for i in range(100):
+    zahl = zahl + 1
+    fizz = zahl / 3
+    buzz = zahl / 5
+    if fizz.is_integer():
+        print('Fizz')
+    elif buzz.is_integer():
+        print('Buzz')
+    else:
+        print(zahl)
